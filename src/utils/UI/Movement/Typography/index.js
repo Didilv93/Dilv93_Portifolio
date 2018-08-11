5b86a8814df6ecd0b0f71a02cb998b8a7c15ab20
Didilv93 <diego.silva.lv93@gmail.com>
2018-08-10 22:56:10 -0300

    mais detalhes

--- src/utils/UI/Movement/Typography/index.js
+++ src/utils/UI/Movement/Typography/index.js
@@ -6,7 +6,7 @@ function ModeChoice () {
   const ButonHero = document.getElementsByClassName('scrollButonHero');
   setTimeout(function() {
     ButonHero[0].style.opacity = 1;
-  }, 4000);
+  }, 5000);
   OpacityWords('lineStyle', 1000);
   RotateWords('word', 4000, 4000);
 
